f78b0bbef9f5017c1b00d3d5b08245f76636c5c8
Kikuro12 <165280913+Kikuro12@users.noreply.github.com>
2025-07-10 22:56:31 +0800

    Delete components/ui directory

--- components/ui/button.jsx
+++
@@ -1,7 +0,0 @@
-export function Button({ children, className, size }) {
-  return (
-    <button className={`px-4 py-2 rounded-xl ${className}`}>
-      {children}
-    </button>
-  );
-}
--- components/ui/card.jsx
+++
@@ -1,3 +0,0 @@
-export function Card({ children, className }) {
-  return <div className={`rounded-2xl bg-white p-4 ${className}`}>{children}</div>;
-}
